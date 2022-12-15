50b30563c6e61d26d287a23167f4248ab6329391
靳荣达 <2368741842@qq.com>
2022-12-15 23:55:22 +0800

    购物车案例

---
+++ 购物车案例/data.js
@@ -0,0 +1,37 @@
+const books = [
+    {
+        id: 1,
+        name: '《算法导论》',
+        data: '2006-9',
+        price: 85.00,
+        count: 1
+    },
+    {
+        id: 2,
+        name: '《算法导论1》',
+        data: '2007-9',
+        price: 85.00,
+        count: 1
+    },
+    {
+        id: 3,
+        name: '《算法导论2》',
+        data: '2008-9',
+        price: 85.00,
+        count: 1
+    },
+    {
+        id: 4,
+        name: '《算法导论3》',
+        data: '2009-9',
+        price: 85.00,
+        count: 1
+    },
+    {
+        id: 5,
+        name: '《算法导论4》',
+        data: '2010-9',
+        price: 85.00,
+        count: 1
+    },
+]
---
+++ 购物车案例/format.js
@@ -0,0 +1,3 @@
+function formatPrice(price) {
+    return "￥" + Number(price).toFixed(2); // 保留小数点后两位
+}
